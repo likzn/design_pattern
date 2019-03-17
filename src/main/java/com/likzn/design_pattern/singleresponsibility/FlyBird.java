8d1739b69969744d145d3b7c3ea6089bdc4633b9
likzn <532573425@qq.com>
2019-03-17 15:30:14 +0800

    up

---
+++ src/main/java/com/likzn/design_pattern/singleresponsibility/FlyBird.java
@@ -0,0 +1,13 @@
+package com.likzn.design_pattern.singleresponsibility;
+
+/**
+ * @auther: Li jx
+ * @date: 2019/3/17 13:33
+ * @description:
+ */
+public class FlyBird {
+    public void move(String birdName) {
+            System.out.println("用翅膀飞");
+    }
+
+}
---
+++ src/main/java/com/likzn/design_pattern/singleresponsibility/ICourseManager.java
@@ -0,0 +1,14 @@
+package com.likzn.design_pattern.singleresponsibility;
+
+/**
+ * className ICourseManager
+ * description ICourseManager
+ *
+ * @author Li jx
+ * @version 1.0
+ * @date 2019/3/17 13:37
+ */
+public interface ICourseManager {
+    void refund();
+    byte[] subscribe();
+}
